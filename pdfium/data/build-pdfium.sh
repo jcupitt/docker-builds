31cc11fcd160f2f722eb66841891e6d00c79da10
John Cupitt <jcupitt@gmail.com>
2018-12-17 13:47:36 +0000

    pdfium fiddles
    but no success :(

--- pdfium/data/build-pdfium.sh
+++ pdfium/data/build-pdfium.sh
@@ -21,14 +21,34 @@ gclient sync --no-history
 
 cd pdfium
 
+# version of 11 dec 2018 that seems to build OK
+# git fetch origin 407a081da693b7ed877e85be24c3eac9fe831315
+# git fetch origin 1ccf890a09a33b8e49d92f8f8f565645be82fb6b
+# git fetch origin 385bf2e0481230b1f5e50a2627063383bd297451
+
 # Change pdfium library from static to shared
 sed -i.bak 's/^jumbo_static_library("pdfium")/jumbo_component("pdfium")/' BUILD.gn
 
 # Fix symbol visibility – files are built with -fvisiblity=hidden
 sed -i.bak 's/^#define FPDF_EXPORT$/#define FPDF_EXPORT  __attribute__((visibility("default")))/' public/fpdfview.h
 
-# see https://pdfium.googlesource.com/pdfium/
-# and https://github.com/lukas-w/pdfium-build/blob/master/args.gn.linux
+# # git master pdfium built with gcc has several warnings
+# treat_warnings_as_errors = false 
+# is_debug = false 
+# pdf_enable_v8 = false 
+# pdf_enable_xfa = false 
+# pdf_use_skia = false 
+# pdf_use_skia_paths = false 
+# pdf_is_standalone = true 
+# is_component_build = false 
+# clang_use_chrome_plugins = false 
+# # build with gcc ... makes linking simpler
+# is_clang = false 
+# # don't use c++14 features
+# use_cxx11 = true
+
+# for some reason we only get a static library out of it :( I'm sure this used
+# to work
 
 gn gen "${BUILD_RES}" --args='treat_warnings_as_errors = false is_debug = false pdf_enable_v8 = false pdf_enable_xfa = false pdf_use_skia = false pdf_use_skia_paths = false pdf_is_standalone = true is_component_build = false clang_use_chrome_plugins = false is_clang = false use_cxx11 = true'
 
--- pdfium/data/package.sh
+++ pdfium/data/package.sh
@@ -13,13 +13,21 @@ fi
 
 mkdir -p ${TARGET_PDFIUM}
 mkdir -p "${TARGET_PDFIUM}/lib"
-mkdir -p "${TARGET_PDFIUM}/include"
+mkdir -p "${TARGET_PDFIUM}/lib/pkgconfig"
+mkdir -p "${TARGET_PDFIUM}/include/pdfium"
 
-cp out/Release_linux-x64/libpdfium.so  ${TARGET_PDFIUM}/lib
-cp out/Release_linux-x64/libc++.so  ${TARGET_PDFIUM}/lib
-cp out/Release_linux-x64/libicuuc.so  ${TARGET_PDFIUM}/lib
+# libpdfium is built as a thin archive, so it only contains paths to the
+# objects ... repack as a full static library
+base=out/Release_linux-x64/obj
+llvm-ar -t $base/libpdfium.a | \
+  xargs ar rvs ${TARGET_PDFIUM}/lib/libpdfium.a
 
-cp public/*.h ${TARGET_PDFIUM}/include
+cp public/*.h ${TARGET_PDFIUM}/include/pdfium
+
+cp ../../libpdfium.pc.in /tmp/libpdfium.pc
+# ahem not a real version number
+sed -i.bak 's/@VERSION@/1.0/' /tmp/libpdfium.pc
+cp /tmp/libpdfium.pc ${TARGET_PDFIUM}/lib/pkgconfig
 
 if [ -d ${BUILD_PKG} ]; then
     rm -rf ${BUILD_PKG}
