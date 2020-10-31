1d4d994979778191e45f001dc6690b8648ad13e7
John Cupitt <jcupitt@gmail.com>
2020-10-31 18:02:14 +0000

    test for pre-rot

---
+++ msm-fsl6-ubuntu16.04/pre-rotation/copy.sh
@@ -0,0 +1,4 @@
+cp ~/vol/dhcp-derived-data/derived_aug20_recon07/derivatives/sub-CC00692XX17/ses-200301/anat/sub-CC00692XX17_ses-200301_T2w_restore_brain.nii.gz .
+cp ~/vol/dhcp-derived-data/derived_aug20_recon07/derivatives/sub-CC00692XX17/ses-200301/anat/Native/sub-CC00692XX17_ses-200301_left_sphere.surf.gii .
+cp ~/vol/dhcp-derived-data/surface-atlas-jcupitt/schuh-atlas-jan2020/templates/t2w/t42.nii.gz .
+cp ~/vol/dhcp-derived-data/surface-atlas-jcupitt/SurfaceAtlasConstruction/rotational_transforms/week40_toFS_LR_rot.L.txt .
