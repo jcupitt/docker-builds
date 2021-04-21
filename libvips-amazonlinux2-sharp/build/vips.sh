ce04cc69669f9ebed411a07efa161fda46d7b9cc
Nathan Bolam <bolamnathan@gmail.com>
2021-04-21 23:41:06 +1000

    replicate sharp structure completely

--- libvips-amazonlinux2-sharp/build/vips.sh
+++ libvips-amazonlinux2-sharp/build/vips.sh
@@ -396,6 +396,35 @@ build imagemagick
 #   - phpvips
 build vips
 
+rm -rf ${PREFIX}/lib/{pkgconfig,.libs,*.la,cmake}
+
+mkdir ${PREFIX}/lib-filtered
+mv ${PREFIX}/lib/glib-2.0 ${PREFIX}/lib-filtered
+
+function copydeps {
+  local base=$1
+  local dest_dir=$2
+
+  cp -L $base $dest_dir/$base
+  chmod 644 $dest_dir/$base
+
+  local dependencies=$(readelf -d $base | grep NEEDED | awk '{ print $5 }' | tr -d '[]')
+
+  for dep in $dependencies; do
+    base_dep=$(basename $dep)
+
+    [ ! -f "$PWD/$base_dep" ] && echo "$base_dep does not exist in $PWD" && continue
+    echo "$base depends on $base_dep"
+
+    if [ ! -f "$dest_dir/$base_dep" ]; then
+      copydeps $base_dep $dest_dir
+    fi
+  done;
+}
+
+cd ${PREFIX}/lib
+copydeps libvips-cpp.so.42 ${PREFIX}/lib-filtered
+
 cd ${PREFIX}
 
 AOM_VERSION=3.0.0
@@ -470,6 +499,8 @@ printf "{\n\
 printf "\"${PLATFORM}\"" >platform.json
 
 ls -al lib
+rm -rf lib
+mv lib-filtered lib
 
 tar chzf ${PACKAGE}/libvips-${VIPS_VERSION}-${PLATFORM}.tar.gz \
   include \
